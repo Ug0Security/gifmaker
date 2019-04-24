aff19da2b12886536dcb7c1fbe5f4d96e485ca70
Ug0Security <c.vallicari@gmail.com>
2019-04-24 17:59:26 +0200

    Update gifmaker.sh

--- gifmaker.sh
+++ gifmaker.sh
@@ -1,15 +1,27 @@
-total=$(ffprobe -i $1.mp4 -show_format -v quiet | sed -n 's/duration=//p'| xargs printf %.0f)
-div=$(echo $(($total/10)))
+if [ $# -eq 0 ]; then
+    echo -n -e "usage : bash gitmaker.sh video_input "
+    exit 1
+fi
+
+total=$(ffprobe -i $1 -show_format -v quiet | sed -n 's/duration=//p'| xargs printf %.0f)
+
+if [ "$total" -gt "1000" ]; then
+    sec=60
+else
+    sec=10
+fi
+
+div=$(echo $(($total/$sec)))
 nb=`echo "$div + 1" | bc`
-echo "La video $1.mp4 fait $total secondes"
-echo "On capture une photo toutes les 10 secondes ça fait donc $nb photos" 
-for i in $(eval echo "{0..$div}") ;do temps=$(echo $i*10 | bc); ffmpeg -accurate_seek -ss $temps -i $1.mp4   -frames:v 1 period_down_$i.jpg -hide_banner -loglevel quiet; echo "photo $i capturée";done
+echo "La video $1 fait $total secondes"
+echo "On capture une photo toutes les $sec secondes ça fait donc $nb photos" 
+for i in $(eval echo "{0..$div}") ;do temps=$(echo $i*10 | bc); ffmpeg -accurate_seek -ss $temps -i $1   -frames:v 1 photo_temp_$i.jpg -hide_banner -loglevel quiet; echo "photo $i capturée";done
 echo "$nb Photos prises... OK"
 echo "Maintenant on resize"
-mogrify -resize 20% *.jpg
+mogrify -resize 20% photo_temp_*.jpg
 echo "Resize... OK"
 echo "Maintenant on construit le GIF"
-convert -limit memory 3MB -delay 100 -loop 0 *.jpg $1.gif
+convert -limit memory 3MB -delay 100 -loop 0 photo_temp_*.jpg $1.gif
 echo "Gif Créé... OK"
 echo "Maintenant on clean"
-rm *.jpg
+rm photo_temp_*.jpg
